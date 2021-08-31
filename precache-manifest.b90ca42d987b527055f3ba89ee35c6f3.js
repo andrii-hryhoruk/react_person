071bc0bad691b5e9815006564ed7da29204d148c
Andrii Hryhoruk <andriihryhoruk5544@gmail.com>
2021-08-31 14:44:44 +0300

    Updates

---
+++ precache-manifest.b90ca42d987b527055f3ba89ee35c6f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b732008c4fa7db6587c44e4f207fce39",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5e6f37389935005b0707",
+    "url": "./static/css/main.2c5caab5.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e6f37389935005b0707",
+    "url": "./static/js/main.1bfeefcc.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
